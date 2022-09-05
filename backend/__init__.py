b8dd24c78bdcdd29e3444cb102a9e6131ce2b7d5
smarcet <smarcet@gmail.com>
2022-09-05 17:28:20 -0300

    Fixed settings
    Signed-off-by: smarcet <smarcet@gmail.com>

---
+++ backend/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+env = os.getenv('ENV')
+filename = '.env'
+if env:
+    filename = '{filename}.{env}'.format(filename=filename, env=env)
+
+ENV_FILE = os.path.join(CURRENT_PATH, filename)
+load_dotenv(ENV_FILE)
--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import sys
-from dotenv import load_dotenv
 from django.utils.translation import ugettext_lazy as _
 
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -23,21 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 ENV = os.getenv("ENV")
 
-if ENV == 'test':
-    filename = '.env'
-    if ENV:
-        filename = '{filename}.{env}'.format(filename=filename, env=ENV)
-
-    ENV_FILE = os.path.join(CURRENT_PATH, filename)
-    load_dotenv(ENV_FILE)
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if ENV == 'production' else True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", '*').split('|')
 
 # https://docs.djangoproject.com/en/3.0/howto/error-reporting/
 # string should have this format
