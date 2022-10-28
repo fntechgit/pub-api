f3b5400bcd4a7c885c80d832d75710187bee555c
smarcet <smarcet@gmail.com>
2022-10-28 11:54:51 -0300

    Fix on missing param
    Signed-off-by: smarcet <smarcet@gmail.com>

--- api/tasks.py
+++ api/tasks.py
@@ -3,6 +3,7 @@ import logging
 from celery import shared_task
 
 from api.models import RedisWSPubService
+from api.models import SupaBasePubService
 from api.models.feeds_download_service import FeedsDownloadService
 from api.models.feeds_upload_service import FeedsUploadService
 from api.security.access_token_service import AccessTokenService
@@ -13,7 +14,7 @@ def create_model_snapshot(summit_id: int):
     logging.getLogger('api').debug(f'calling create_model_snapshot task for summit {summit_id}...')
 
     feeds_download_service = FeedsDownloadService(AccessTokenService())
-    feeds_upload_service = FeedsUploadService(RedisWSPubService())
+    feeds_upload_service = FeedsUploadService(SupaBasePubService, RedisWSPubService())
 
     feeds_download_service.download(summit_id)
     feeds_upload_service.upload(summit_id)
